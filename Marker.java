c9b5b049d6ef6cb8c856e34fe98e98f68e79875f
Harry Sadleir <32585089+HarrySadleir@users.noreply.github.com>
2019-06-11 11:23:19 -0700

    last update before pull
    it works, and added question difficulty

--- Marker.java
+++ Marker.java
@@ -9,18 +9,45 @@ public class Marker {
         try{ 
             Scanner input = new Scanner(f);            
             String[] ans = null;
-            ArrayList<Student> = new ArrayList<Student>();
+            ArrayList<Student> students = new ArrayList<Student>();
             while(input.hasNext()) {
                 String str = input.nextLine();
                 String[] vals = str.split(",");
                 if(vals[0].equals("Answer Key")) ans = vals;
                 else {
-                    Student s = new Student();
+                    students.add(new Student(vals));
                 }
             }
+
+            printScores(students, ans);
+            printQuestionDifficulty(students, ans);
         } 
         catch (FileNotFoundException e) {   
             e.printStackTrace();
         }
     }
+
+    public static void printScores(ArrayList<Student> students, String[] ans) {
+        System.out.println("Student Scores:  \n");
+        for(Student s: students) {
+            System.out.println(s.getName() + ": " + s.getScore(ans));
+        }
+        System.out.println("");
+    }
+    
+    public static void printQuestionDifficulty(ArrayList<Student> students, String[] key) {
+        System.out.println("Correct Answers Per Question:  \n");
+        int questions = students.get(0).getQuestions();
+        
+        for(int i = 0; i < questions; i++) {
+            
+            int corrects = 0;
+            for(Student s: students) {
+                if(s.getAnswer(i) == Integer.parseInt(key[i+1])) corrects++;
+            }
+            
+            System.out.println((i+1) + ": " + (corrects) + "/" + students.size());
+        }
+        System.out.println("");
+    } 
 }
--- Student.java
+++ Student.java
@@ -2,24 +2,31 @@ import java.util.ArrayList;
 public class Student{
     private ArrayList<String> answers;
     private String name;
+    private int questions;
     
     Student(String[] vals) {
         name = vals[0];
+        answers = new ArrayList<String>();
         for(int i = 1; i < vals.length; i++) {
             answers.add(vals[i]);
         }
+        questions = answers.size();
     }
     
-    public String getScore(ArrayList<String> key) {
+    public String getScore(String[] key) {
         int num = 0;
-        int den = 0;
-        for(int i = 0; i < answers.size(); i++) {
-          if(answers.get(i).equals(key.get(i))) num++;
-          den++;
+        
+        for(int i = 1; i < answers.size()+1; i++) {
+          if(answers.get(i-1).equals(key[i])) num++;
         } 
         
-        return num+"/"+den;
+        return num+"/"+questions;
     }
+    
+    public int getQuestions() {
+        return questions;
+    }
+    
     
     public String getName() {
         return name;
